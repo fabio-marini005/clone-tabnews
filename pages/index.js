37e73d036e877794fea69ab62409a548945ce7c8
Fabio Marini <53901933+fabio-marini005@users.noreply.github.com>
2026-06-01 11:59:55 +0000

    alteração fraseimpactante

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Impacto</h1>
+    return <h1>Você é a mulher mais linda que eu já conheci, a pessoa que transformou meus dias e deu ainda mais sentido à minha vida. Ao seu lado vivi momentos inesquecíveis, construímos nossa família e compartilhamos sonhos, desafios e conquistas.
+
+Cada dia que passa tenho mais certeza de que é com você que eu quero viver todos os próximos capítulos da minha história.
+
+Por isso, eu quero te fazer uma pergunta que vem do fundo do meu coração:
+
+Você aceita casar comigo (novamente) e continuar sendo o amor da minha vida para sempre? ❤️💍
+
+Eu te amo mais do que as palavras conseguem explicar.</h1>
 }
 
 export default Home; 
